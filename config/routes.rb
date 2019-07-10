1601ea28d92da60fc78a151eef4ef47d5a2df53d
takumi aramaki <armktk0329m@gmail.com>
2019-07-10 21:07:17 +0900

    ルートページの設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
 
+  get '', to: 'pictures#index'
+
+
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
